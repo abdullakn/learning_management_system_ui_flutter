6382e6d92aef90f5942a3e693db50324a42e5f8f
abdullakn <abdudebanjazz@gmail.com>
2022-03-25 11:10:40 +0530

    home page design completed

---
+++ lib/color_Scheme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+
+const Color lightBlue = Color(0xffdbf0f1);
+const Color darkBlue = Color(0xff39888e);
+const Color yellow = Color(0xffffe9a7);
+const Color pink = Color(0xfff1e7f5);
